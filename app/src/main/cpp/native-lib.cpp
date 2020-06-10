ce02f31ed065b044b09703777178ea603a741df8
havejin <2geni2@gmail.com>
2020-06-10 16:15:59 +0900

    열 글자 분리 인식 성공!!

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,7 +5,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <iostream>
 #include <opencv2/core/core.hpp>
-#include<string>
+#include <string>
 
 using namespace cv;
 using namespace std;
@@ -204,11 +204,11 @@ Java_com_example_font_1opencv_activity_1sub_101_opencv_101(JNIEnv *env, jobject 
 
 
     //이진화
-    Mat mask = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5), cv::Point(1, 1)); //delite연산 kernal 크기
+    Mat mask = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(7, 7), cv::Point(1, 1)); //delite연산 kernal 크기
     //이미지를 부드럽게 만듦 (입력이미지,출력이미지,...)
     GaussianBlur(input_gray_image, input_gray_image, cv::Point(5, 5), 0);
     //이미지를 이진화 (입력이미지,출력이미지,...)
-    adaptiveThreshold(input_gray_image, result_binary_image, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 29, 3);
+    adaptiveThreshold(input_gray_image, result_binary_image, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 27, 4);
     //이미지 잡음 제거 (입력이미지,출력이미지...)
     morphologyEx(result_binary_image, result_binary_image, cv::MORPH_CLOSE, kernel); //close
     //색 반전: deliate연산을 위해
@@ -216,59 +216,76 @@ Java_com_example_font_1opencv_activity_1sub_101_opencv_101(JNIEnv *env, jobject 
     //팽창 연산, 이미지의 하얀부분을 팽창시킨다 (입력이미지, 출력이미지,...,반복횟수)
     dilate(result_binary_image, result_binary_image, mask, cv::Point(-1, -1), 5);
 
-    int cnt = 0; // 글자 구별해서 저장하기 위해
-
     //이진화 이미지에서 외곽선 찾기
     std::vector<vector<Point>> contours;
     std::vector<Vec4i> hierarchy;
 
     findContours(result_binary_image, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-    //외곽선에 맞춰 사각형 그리기
+    Rect roiRect[10];//알맞은 사각형 배열
+    int i = 0; // 글자 구별해서 저장하기 위해
+
+    //외곽선에 맞춰 사각형 찾기
     if (contours.size() > 0) {
         for (int idx = 0; idx < contours.size(); idx++) {
             Rect rect = boundingRect(contours[idx]);
             //알맞은 사각형 조건 (수정)
-            if (rect.width < 2000 && rect.height > 200 && rect.height * rect.width > 160000) {
+            if (rect.width > 720 && rect.width < 2000 && rect.height > 190 && rect.height * rect.width > 130000) {
                 //rectangle(input_origin_image, Point(rect.x, rect.y), Point(rect.x + rect.width, rect.y + rect.height), Scalar(0, 255, 0), 3);
-                switch(cnt)
-                {
-                    case 0:
-                        roi1 = input_origin_image(rect); //CROP
-                        break;
-                    case 1:
-                        roi2 = input_origin_image(rect); //CROP
-                        break;
-                    case 2:
-                        roi3 = input_origin_image(rect); //CROP
-                        break;
-                    case 3:
-                        roi4 = input_origin_image(rect); //CROP
-                        break;
-                    case 4:
-                        roi5 = input_origin_image(rect); //CROP
-                        break;
-                    case 5:
-                        roi6 = input_origin_image(rect); //CROP
-                        break;
-                    case 6:
-                        roi7 = input_origin_image(rect); //CROP
-                        break;
-                    case 7:
-                        roi8 = input_origin_image(rect); //CROP
-                        break;
-                    case 8:
-                        roi9 = input_origin_image(rect); //CROP
-                        break;
-                    case 9:
-                        roi10 = input_origin_image(rect); //CROP
-                        break;
-                    default:
-                        //other_roi[cnt] = input_origin_image(rect);
-                        break;
-                }
-                cnt++;
+                roiRect[i] = rect;
+                i++;
             }
         }
+    }
+
+    //순서대로 정렬
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10 - i - 1; j++) {
+            if (roiRect[j].x > roiRect[j+1].x) {
+                Rect bff = roiRect[j];
+                roiRect[j] = roiRect[j + 1];
+                roiRect[j + 1] = bff;
+            }
+        }
+    }
+
+    //저장
+    for (int i = 0; i < 10; i++) {
+        switch(i) {
+            case 0:
+                roi1 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 1:
+                roi2 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 2:
+                roi3 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 3:
+                roi4 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 4:
+                roi5 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 5:
+                roi6 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 6:
+                roi7 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 7:
+                roi8 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 8:
+                roi9 = input_origin_image(roiRect[i]); //CROP
+                break;
+            case 9:
+                roi10 = input_origin_image(roiRect[i]); //CROP
+                break;
+            default:
+                //other_roi[cnt] = input_origin_image(rect);
+                break;
+        }
+
     }
 }
